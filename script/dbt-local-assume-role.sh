fdf5771db7a4d7c531c9c63af68751db102298ff
nijigen-plot <quark_hardcore@yahoo.co.jp>
2024-11-28 18:43:36 +0900

    session_nameを変更

--- script/dbt-local-assume-role.sh
+++ script/dbt-local-assume-role.sh
@@ -2,7 +2,7 @@
 
 # AWS_ACCOUNT_IDは.envrcで定義してください
 ROLE_ARN="arn:aws:iam::$AWS_ACCOUNT_ID:role/LocalDevelopmentAssumeRole"
-SESSION_NAME="LocalDevelopmentAssumeRole"
+SESSION_NAME="dbt-local"
 PROFILE_NAME="dbt-local"
 
 CREDENTIALS=$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "$SESSION_NAME" )
