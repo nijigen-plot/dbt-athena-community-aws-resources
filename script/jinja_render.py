becabc8773b74014d0aee1a06e2754a670d1e59e
nijigen-plot <quark_hardcore@yahoo.co.jp>
2024-12-02 22:19:19 +0900

    stepfunctionsの変換も追加

--- script/jinja_render.py
+++ script/jinja_render.py
@@ -7,12 +7,14 @@ env = Environment(loader=FileSystemLoader("."))
 # cloudformation yaml template
 cfn_template = env.get_template("./aws/cloudformation/dbt-infrastructure.j2.yml")
 buildspec_template = env.get_template("./buildspec.j2.yml")
+stepfunctions_template = env.get_template("./aws/stepfunctions/dbt_batch.j2.yml")
 
 params = {
     "dbt_table_resource_s3_bucket": os.getenv("DBT_TABLE_RESOURCE_S3_BUCKET"),
     "query_output_s3_bucket": os.getenv("QUERY_OUTPUT_S3_BUCKET"),
     "data_pipeline_resource_s3_bucket": os.getenv("DATA_PIPELINE_RESOURCE_S3_BUCKET"),
     "slack_webhook_url_secrets_name": os.getenv("SLACK_WEBHOOK_URL_SECRETS_NAME"),
+    "slack_webhook_url_channel_name": os.getenv("SLACK_WEBHOOK_URL_CHANNEL_NAME"),
     "cloudformation_policy_name": os.getenv("CLOUDFORMATION_POLICY_NAME"),
     "cloudformation_role_name": os.getenv("CLOUDFORMATION_ROLE_NAME"),
     "local_dev_assume_role_bearer_user_name": os.getenv(
@@ -30,3 +32,6 @@ with open(
     f'{os.getenv("REPOSITORY_DIR", os.path.expanduser("."))}/buildspec.yml', "w"
 ) as file:
     file.write(buildspec_template.render(params) + "\n")
+with open(
+    f'{os.getenv("REPOSITORY_DIR", os.path.expanduser("."))}/aws/stepfunctions/dbt_batch.yml', "w") as file:
+    file.write(stepfunctions_template.render(params) + "\n")
